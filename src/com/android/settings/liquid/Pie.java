DirtyUnicorns/android_packages_apps_Settings

Bring back more PIE targets [2/2]
- Replaced App window with Torch pie trigger

Change-Id: Ie36c7af28b55ac21859ac3dc466196feb9068372

src/com/android/settings/du/Pie.java
@@ -46,8 +46,6 @@ public class Pie extends SettingsPreferenceFragment
     private static final String PIE_ANGLE = "pie_angle";
     private static final String PIE_GAP = "pie_gap";
     private static final String PIE_NOTIFICATIONS = "pie_notifications";
-    private static final String PIE_MENU = "pie_menu";
-    private static final String PIE_SEARCH = "pie_search";
     private static final String PIE_CENTER = "pie_center";
     private static final String PIE_STICK = "pie_stick";
 
@@ -59,8 +57,6 @@ public class Pie extends SettingsPreferenceFragment
     private ListPreference mPieGap;
     private CheckBoxPreference mPieNotifi;
     private SwitchPreference mPieControls;
-    private CheckBoxPreference mPieMenu;
-    private CheckBoxPreference mPieSearch;
     private CheckBoxPreference mPieCenter;
     private CheckBoxPreference mPieStick;
 
@@ -95,7 +91,7 @@ public class Pie extends SettingsPreferenceFragment
 
         mPieStick = (CheckBoxPreference) prefSet.findPreference(PIE_STICK);
         mPieStick.setChecked(Settings.System.getInt(mResolver,
-                Settings.System.PIE_STICK, 1) != 0);
+                Settings.System.PIE_STICK, 0) != 0);
 
         mPieGravity = (ListPreference) prefSet.findPreference(PIE_GRAVITY);
         int pieGravity = Settings.System.getInt(mResolver,
@@ -141,13 +137,6 @@ public class Pie extends SettingsPreferenceFragment
         mPieAngle.setValue(String.valueOf(pieAngle));
         mPieAngle.setOnPreferenceChangeListener(this);
 
-        mPieMenu = (CheckBoxPreference) prefSet.findPreference(PIE_MENU);
-        mPieMenu.setChecked(Settings.System.getInt(mResolver,
-                Settings.System.PIE_MENU, 1) != 0);
-
-        mPieSearch = (CheckBoxPreference) prefSet.findPreference(PIE_SEARCH);
-        mPieSearch.setChecked(Settings.System.getInt(mResolver,
-                Settings.System.PIE_SEARCH, 1) != 0);
     }
 
     @Override
@@ -156,20 +145,14 @@ public class Pie extends SettingsPreferenceFragment
             Settings.System.putInt(mResolver,
                     Settings.System.PIE_NOTIFICATIONS,
                     mPieNotifi.isChecked() ? 1 : 0);
-        } else if (preference == mPieMenu) {
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PIE_MENU,
-                    mPieMenu.isChecked() ? 1 : 0);
-        } else if (preference == mPieSearch) {
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PIE_SEARCH,
-                    mPieSearch.isChecked() ? 1 : 0);
         } else if (preference == mPieCenter) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PIE_CENTER, mPieCenter.isChecked() ? 1 : 0);
+                    Settings.System.PIE_CENTER,
+                    mPieCenter.isChecked() ? 1 : 0);
         } else if (preference == mPieStick) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PIE_STICK, mPieStick.isChecked() ? 1 : 0);
+                    Settings.System.PIE_STICK,
+                    mPieStick.isChecked() ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
@@ -180,7 +163,7 @@ public class Pie extends SettingsPreferenceFragment
                     Settings.System.PIE_CONTROLS,
                     (Boolean) newValue ? 1 : 0);
             if (mPieControls.isChecked()) {
-                Toast.makeText(getActivity(), "NO, we're not gonna add Hover!",
+                Toast.makeText(getActivity(), "No, We're not going to add Hover!!!",
                         Toast.LENGTH_LONG).show();
             }
             return true;

src/com/android/settings/du/PieTargets.java
@@ -1,0 +1,139 @@
+/*
+ * Copyright (C) 2012 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.du;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieTargets extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String PIE_MENU = "pie_menu";
+    private static final String PIE_SEARCH = "pie_search";
+    private static final String PIE_LASTAPP = "pie_lastapp";
+    private static final String PIE_KILLTASK = "pie_killtask";
+    private static final String PIE_TORCH = "pie_torch";
+    private static final String PIE_ACTNOTIF = "pie_actnotif";
+    private static final String PIE_POWER = "pie_power";
+
+    private CheckBoxPreference mPieMenu;
+    private CheckBoxPreference mPieSearch;
+    private CheckBoxPreference mPieLastApp;
+    private CheckBoxPreference mPieKillTask;
+    private CheckBoxPreference mPieTorch;
+    private CheckBoxPreference mPieActNotif;
+    private CheckBoxPreference mPiePower;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.pie_targets);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setIcon(R.drawable.ic_settings_dirt);
+
+        Context context = getActivity();
+        mResolver = context.getContentResolver();
+
+        mPieMenu = (CheckBoxPreference) prefSet.findPreference(PIE_MENU);
+        mPieMenu.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PIE_MENU, 1) != 0);
+
+        mPieSearch = (CheckBoxPreference) prefSet.findPreference(PIE_SEARCH);
+        mPieSearch.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PIE_SEARCH, 0) != 0);
+
+        mPieLastApp = (CheckBoxPreference) prefSet.findPreference(PIE_LASTAPP);
+        mPieLastApp.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PIE_LAST_APP, 0) != 0);
+
+        mPieKillTask = (CheckBoxPreference) prefSet.findPreference(PIE_KILLTASK);
+        mPieKillTask.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PIE_KILL_TASK, 0) != 0);
+
+        mPieTorch = (CheckBoxPreference) prefSet.findPreference(PIE_TORCH);
+        mPieTorch.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PIE_TORCH, 0) != 0);
+
+        mPieActNotif = (CheckBoxPreference) prefSet.findPreference(PIE_ACTNOTIF);
+        mPieActNotif.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PIE_ACT_NOTIF, 0) != 0);
+
+        mPiePower = (CheckBoxPreference) prefSet.findPreference(PIE_POWER);
+        mPiePower.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PIE_POWER, 0) != 0);
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mPieMenu) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PIE_MENU,
+                    mPieMenu.isChecked() ? 1 : 0);
+        } else if (preference == mPieSearch) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PIE_SEARCH,
+                    mPieSearch.isChecked() ? 1 : 0);
+        } else if (preference == mPieLastApp) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PIE_LAST_APP,
+                    mPieLastApp.isChecked() ? 1 : 0);
+        } else if (preference == mPieKillTask) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PIE_KILL_TASK,
+                    mPieKillTask.isChecked() ? 1 : 0);
+        } else if (preference == mPieTorch) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PIE_TORCH,
+                    mPieTorch.isChecked() ? 1 : 0);
+        } else if (preference == mPieActNotif) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PIE_ACT_NOTIF,
+                    mPieActNotif.isChecked() ? 1 : 0);
+        } else if (preference == mPiePower) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PIE_POWER,
+                    mPiePower.isChecked() ? 1 : 0);
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}

Jaiglissechef-i9100/android_packages_apps_Settings-1

Conflicts:
	AndroidManifest.xml
	res/values/custom_strings.xml
	res/xml/pie.xml
	src/com/android/settings/liquid/Pie.java

src/com/android/settings/liquid/Pie.java
@@ -45,8 +45,6 @@ public class Pie extends SettingsPreferenceFragment
     private static final String PA_PIE_ANGLE = "pa_pie_angle";
     private static final String PA_PIE_GAP = "pa_pie_gap";
     private static final String PA_PIE_NOTIFICATIONS = "pa_pie_notifications";
-    private static final String PA_PIE_MENU = "pa_pie_menu";
-    private static final String PA_PIE_SEARCH = "pa_pie_search";
     private static final String PA_PIE_CENTER = "pa_pie_center";
     private static final String PA_PIE_STICK = "pa_pie_stick";
 
@@ -58,8 +56,6 @@ public class Pie extends SettingsPreferenceFragment
     private ListPreference mPieGap;
     private CheckBoxPreference mPieNotifi;
     private SwitchPreference mPieControls;
-    private CheckBoxPreference mPieMenu;
-    private CheckBoxPreference mPieSearch;
     private CheckBoxPreference mPieCenter;
     private CheckBoxPreference mPieStick;
 
@@ -91,7 +87,7 @@ public class Pie extends SettingsPreferenceFragment
 
         mPieStick = (CheckBoxPreference) prefSet.findPreference(PA_PIE_STICK);
         mPieStick.setChecked(Settings.System.getInt(mResolver,
-                Settings.System.PA_PIE_STICK, 1) != 0);
+                Settings.System.PA_PIE_STICK, 0) != 0);
 
         mPieGravity = (ListPreference) prefSet.findPreference(PA_PIE_GRAVITY);
         int pieGravity = Settings.System.getInt(mResolver,
@@ -137,13 +133,6 @@ public class Pie extends SettingsPreferenceFragment
         mPieAngle.setValue(String.valueOf(pieAngle));
         mPieAngle.setOnPreferenceChangeListener(this);
 
-        mPieMenu = (CheckBoxPreference) prefSet.findPreference(PA_PIE_MENU);
-        mPieMenu.setChecked(Settings.System.getInt(mResolver,
-                Settings.System.PA_PIE_MENU, 1) != 0);
-
-        mPieSearch = (CheckBoxPreference) prefSet.findPreference(PA_PIE_SEARCH);
-        mPieSearch.setChecked(Settings.System.getInt(mResolver,
-                Settings.System.PA_PIE_SEARCH, 1) != 0);
     }
 
     @Override
@@ -152,20 +141,14 @@ public class Pie extends SettingsPreferenceFragment
             Settings.System.putInt(mResolver,
                     Settings.System.PA_PIE_NOTIFICATIONS,
                     mPieNotifi.isChecked() ? 1 : 0);
-        } else if (preference == mPieMenu) {
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PA_PIE_MENU,
-                    mPieMenu.isChecked() ? 1 : 0);
-        } else if (preference == mPieSearch) {
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PA_PIE_SEARCH,
-                    mPieSearch.isChecked() ? 1 : 0);
         } else if (preference == mPieCenter) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PA_PIE_CENTER, mPieCenter.isChecked() ? 1 : 0);
+                    Settings.System.PA_PIE_CENTER,
+                    mPieCenter.isChecked() ? 1 : 0);
         } else if (preference == mPieStick) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.PA_PIE_STICK, mPieStick.isChecked() ? 1 : 0);
+                    Settings.System.PA_PIE_STICK,
+                    mPieStick.isChecked() ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
@@ -176,7 +159,7 @@ public class Pie extends SettingsPreferenceFragment
                     Settings.System.PA_PIE_CONTROLS,
                     (Boolean) newValue ? 1 : 0);
             if (mPieControls.isChecked()) {
-                Toast.makeText(getActivity(), "NO, we're not gonna add Hover!",
+                Toast.makeText(getActivity(), "No, We're not going to add Hover!!!",
                         Toast.LENGTH_LONG).show();
             }
             return true;

src/com/android/settings/liquid/PieTargets.java
@@ -1,0 +1,139 @@
+/*
+ * Copyright (C) 2012 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.liquid;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PieTargets extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String PIE_MENU = "pie_menu";
+    private static final String PIE_SEARCH = "pie_search";
+    private static final String PIE_LASTAPP = "pie_lastapp";
+    private static final String PIE_KILLTASK = "pie_killtask";
+    private static final String PIE_TORCH = "pie_torch";
+    private static final String PIE_ACTNOTIF = "pie_actnotif";
+    private static final String PIE_POWER = "pie_power";
+
+    private CheckBoxPreference mPieMenu;
+    private CheckBoxPreference mPieSearch;
+    private CheckBoxPreference mPieLastApp;
+    private CheckBoxPreference mPieKillTask;
+    private CheckBoxPreference mPieTorch;
+    private CheckBoxPreference mPieActNotif;
+    private CheckBoxPreference mPiePower;
+
+    private ContentResolver mResolver;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.pie_targets);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setIcon(R.drawable.ic_settings_dirt);
+
+        Context context = getActivity();
+        mResolver = context.getContentResolver();
+
+        mPieMenu = (CheckBoxPreference) prefSet.findPreference(PIE_MENU);
+        mPieMenu.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PA_PIE_MENU, 1) != 0);
+
+        mPieSearch = (CheckBoxPreference) prefSet.findPreference(PIE_SEARCH);
+        mPieSearch.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PA_PIE_SEARCH, 0) != 0);
+
+        mPieLastApp = (CheckBoxPreference) prefSet.findPreference(PIE_LASTAPP);
+        mPieLastApp.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PA_PIE_LAST_APP, 0) != 0);
+
+        mPieKillTask = (CheckBoxPreference) prefSet.findPreference(PIE_KILLTASK);
+        mPieKillTask.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PA_PIE_KILL_TASK, 0) != 0);
+
+        mPieTorch = (CheckBoxPreference) prefSet.findPreference(PIE_TORCH);
+        mPieTorch.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PA_PIE_TORCH, 0) != 0);
+
+        mPieActNotif = (CheckBoxPreference) prefSet.findPreference(PIE_ACTNOTIF);
+        mPieActNotif.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PA_PIE_ACT_NOTIF, 0) != 0);
+
+        mPiePower = (CheckBoxPreference) prefSet.findPreference(PIE_POWER);
+        mPiePower.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.PA_PIE_POWER, 0) != 0);
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mPieMenu) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PA_PIE_MENU,
+                    mPieMenu.isChecked() ? 1 : 0);
+        } else if (preference == mPieSearch) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PA_PIE_SEARCH,
+                    mPieSearch.isChecked() ? 1 : 0);
+        } else if (preference == mPieLastApp) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PA_PIE_LAST_APP,
+                    mPieLastApp.isChecked() ? 1 : 0);
+        } else if (preference == mPieKillTask) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PA_PIE_KILL_TASK,
+                    mPieKillTask.isChecked() ? 1 : 0);
+        } else if (preference == mPieTorch) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PA_PIE_TORCH,
+                    mPieTorch.isChecked() ? 1 : 0);
+        } else if (preference == mPieActNotif) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PA_PIE_ACT_NOTIF,
+                    mPieActNotif.isChecked() ? 1 : 0);
+        } else if (preference == mPiePower) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.PA_PIE_POWER,
+                    mPiePower.isChecked() ? 1 : 0);
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}