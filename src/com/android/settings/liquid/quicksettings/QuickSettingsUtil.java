dskim9752/android_packages_apps_Settings

Quick Settings: Weather tile
You can check the weather to make it easier.

Dokdo Project - Gwon Hyeok

src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -45,6 +45,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_SYNC;
 import static com.android.internal.util.cm.QSConstants.TILE_TORCH;
 import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
+import static com.android.internal.util.cm.QSConstants.TILE_WEATHER;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 
@@ -161,6 +162,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_INTERNALMEMORY, R.string.internal_memory_tile,
                 "com.android.systemui:drawable/ic_qs_internalmemory"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_WEATHER, R.string.tile_weather,
+                "com.android.systemui:drawable/weather_preferences"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {

Jaiglissechef-i9100/android_packages_apps_Settings-1

Conflicts:
	res/values-ko/dokdo_strings.xml
	res/values/dokdo_strings.xml
	src/com/android/settings/quicksettings/QuickSettingsUtil.java
	res/values/ose_strings.xml
	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -63,6 +63,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_ONTHEGO;
 import static com.android.internal.util.liquid.QSConstants.TILE_PROFILE;
 import static com.android.internal.util.liquid.QSConstants.TILE_HOVER;
 import static com.android.internal.util.liquid.QSConstants.TILE_REMOTEDISPLAY;
+import static com.android.internal.util.liquid.QSConstants.TILE_WEATHER;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -199,6 +200,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_HOVER, R.string.title_tile_hover,
                 "com.android.systemui:drawable/ic_qs_hover_on"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_WEATHER, R.string.tile_weather,
+                "com.android.systemui:drawable/weather_preferences"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {